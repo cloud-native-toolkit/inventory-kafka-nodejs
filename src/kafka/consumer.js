4ca29a6e862b2b4b07f6863d949d043c9068f5f9
Bryan Kribbs <bkribbs15@gmail.com>
2021-02-03 12:50:31 -0600

    first files.

---
+++ src/kafka/consumer.js
@@ -0,0 +1,34 @@
+const { Kafka } = require('kafkajs');
+const config = require('../config/kafkaConnection');
+// 1.Instantiating kafka
+const kafka = new Kafka(config);
+// 2.Creating Kafka Consumer and passing group ID.
+const consumer = kafka.consumer({ groupId: 'medium-group-01' });
+const runConsumer = async () => {
+    try {
+        await consumer.connect()
+        await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+        while(true){
+            await consumer.run({
+                eachMessage: async ({ topic, partition, message }) => {
+                    console.log({ "Doing something with the message": topic, partition, message });
+                    },
+                })
+        }
+    }
+}
+const runConsumer = async () => {
+        // 3.Connecting consumer to kafka broker.
+    await consumer.connect()
+// 4.Subscribing to a topic in order to receive messages/data.
+    await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+// 5. Sending an action to be handled for each message RECEIVED.
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log({ "Doing something with the message": topic, partition, message });
+            },
+        })
+    await consumer.disconnect();
+}
+
+runConsumer();
---
+++ src/kafka/producer.js
@@ -0,0 +1,24 @@
+const { Kafka } = require('kafkajs');
+const config = require('../config/kafkaConnection');
+// 1.Instantiating kafka
+const kafka = new Kafka(config);
+// 2.Creating Kafka Producer
+const producer = kafka.producer();
+const runProducer = async () => {
+    const message = {
+        nTransOrderID: 1000,
+        sTransOrderCode: "TO-101212"
+    }
+// 3.Connecting producer to kafka broker.
+await producer.connect()
+await producer.send({
+    topic: 'test-topic',
+    messages:
+        [{ value: JSON.stringify(message) }],
+    })
+console.log('Message Produced');
+await producer.disconnect()
+}
+
+console.log('Sending Messages');
+runProducer();
