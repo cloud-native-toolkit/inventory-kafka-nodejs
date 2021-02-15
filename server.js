cd0fae306e0fe29ac5a765bf0f0045dd80a12e4d
Bryan Kribbs <bkribbs15@gmail.com>
2021-02-15 09:07:34 -0600

    Standard installed

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const kafkaFunctions = require('./src/kafka/producer.js');
+const express = require('express')
+const app = express()
+const port = 3000
+const kafkaFunctions = require('./src/kafka/producer.js')
 
-const swaggerUi = require('swagger-ui-express'),
-    swaggerDocument = require('./swagger.json');
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-app.get('/', function(req, res) {
-  res.redirect('/api-docs');
-});
+app.get('/', function (req, res) {
+  res.redirect('/api-docs')
+})
 
-app.post('/inventory/update', (req, res) => { 
-  console.log('Inventory Update');
-  console.log("request", req);
-  console.log("KAFKA", kafkaFunctions);
-  res.send("POST Request Called"); 
-});
+app.post('/inventory/update', (req, res) => {
+  console.log('Inventory Update')
+  console.log(req)
+  console.log('KAFKA', kafkaFunctions)
+  kafkaFunctions
+  res.send('Inventory Update Called')
+})
 
-app.get('/api/test', (req, res) => { 
-  console.log('TEST');
-  res.send("POST Request Called"); 
-}) 
+app.get('/api/test', (req, res) => {
+  console.log('TEST')
+  res.send('TEST Request Called')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- src/kafka/producer.js
+++ src/kafka/producer.js
@@ -1,23 +1,23 @@
-const { Kafka } = require('kafkajs');
-const config = require('../config/kafkaConnection');
+const { Kafka } = require('kafkajs')
+const config = require('../config/kafkaConnection')
 // 1.Instantiating kafka
-const kafka = new Kafka(config);
+const kafka = new Kafka(config)
 // 2.Creating Kafka Producer
-const producer = kafka.producer();
-async function runProducer() {
-    const message = {
-        nTransOrderID: 1000,
-        sTransOrderCode: "TO-101212"
-    }
-// 3.Connecting producer to kafka broker.
-await producer.connect()
-await producer.send({
+const producer = kafka.producer()
+
+async function runProducer () {
+  const message = {
+    nTransOrderID: 1000,
+    sTransOrderCode: 'TO-101212'
+  }
+  await producer.connect()
+  await producer.send({
     topic: 'test-topic',
     messages:
-        [{ value: JSON.stringify(message) }],
-    })
-console.log('Message Produced');
-await producer.disconnect()
+            [{ value: JSON.stringify(message) }]
+  })
+  console.log('Message Produced')
+  await producer.disconnect()
 }
 
-module.exports = runProducer;
+module.exports = runProducer
