3dbc840e0d046dbf5c00275e0b31932b6e1c4379
Bryan Kribbs <bkribbs15@gmail.com>
2021-03-02 21:41:40 -0600

    Working Health Check

--- server.js
+++ server.js
@@ -1,37 +1,104 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const port = 3000
-const kafkaFunctions = require('./src/kafka/producer.js')
-
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger.json')
-const expressJSDocSwagger = require('express-jsdoc-swagger');
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(express.json());
+const port = process.env.PORT || 3000
+import runProducer from './src/kafka/producer.js'
 
-const options = {
-  definition: {
-    "openapi": "3.0.1",
-    "info": {
-       "title": "Inventory Service with Kafka",
-       "description": "API for updating the Inventory Service.",
-       "version": "1.0.0"
-    },
-    "servers": [
-       {
-          "url": "http://localhost:3000"
-       }
-    ],
+import swaggerUi  from 'swagger-ui-express';
+//const swaggerDocument = require('./swagger.json')
+import swaggerJsdoc from 'swagger-jsdoc';
+const swaggerDefinition = {
+  "info": {
+    "title": "Inventory Service with Kafka",
+    "description": "API for updating the Inventory Service.",
+    "version": "1.0.0"
   },
-  apis: ['./src/routes*.js'], // files containing annotations as above
+    host: `localhost:${port}`, // Host (optional)
+    basePath: '/', // Base path (optional)
+  };
+  // Options for the swagger docs
+const options = {
+  // Import swaggerDefinitions
+  swaggerDefinition,
+  // Path to the API docs
+  // Note that this path is relative to the current directory from which the Node.js is ran, not the application itself.
+  apis: ['./server.js']
 };
+const swaggerSpec = await swaggerJsdoc(options);
 
-const openapiSpecification = await swaggerJsdoc(options);
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.get('/', function (req, res) {
   res.redirect('/api-docs')
 })
+
+/**
+ * @swagger
+ * definitions:
+ *   Item:
+ *     required:
+ *       - id
+ *       - name
+ *       - stock
+ *       - price
+ *       - manufacturer
+ *     properties:
+ *       id:
+ *         type: integer
+ *       name:
+ *         type: string
+ *       stock:
+ *         type: integer
+ *       price:
+ *         type: integer
+ *         format: double
+ *       manufacturer:
+ *         type: string
+ */
+
+/**
+   * @swagger
+   * /api/healthcheck:
+   *   get:
+   *     description: Returns a Response for the HealthCheck
+   *     responses:
+   *       200:
+   *         description: OK
+   */
+
+app.get('/api/healthcheck', function (req, res) {
+  console.log('HC');
+  const healthcheck = {
+		uptime: process.uptime(),
+		message: 'OK',
+		timestamp: Date.now()
+	};
+	try {
+		res.send(healthcheck);
+	} catch (e) {
+		healthcheck.message = e;
+		res.status(503).send();
+	}
+})
+
+/**
+   * @swagger
+   * /inventory/update:
+   *   post:
+   *     summary: Updates the Inventory using Kafka and CloudEvents
+   *     requestBody:
+   *      content:
+   *        application/json:
+   *          schema:
+   *            $ref: '#/definitions/Item'
+   *     responses:
+   *       200:
+   *         description: Inventory Updated
+   *       501:
+   *         description: Error with Request
+   *     tags:
+   *      - Inventory Actions
+   */
+
 
 app.post("/inventory/update", (req, res) => {
   try {
@@ -61,13 +128,13 @@ app.post("/inventory/update", (req, res) => {
       return;
     }
     const messageOrigin = req.headers.host + req.url;
-    kafkaFunctions(req.body, messageOrigin);
+    runProducer(req.body, messageOrigin);
     res.send('Inventory Update Sent')
   } catch (error) {
     res.status(500).json(error);
   }
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+const server = app.listen(port, () => {
+  console.log('Example app listening at http://localhost:%s', port);
 })
