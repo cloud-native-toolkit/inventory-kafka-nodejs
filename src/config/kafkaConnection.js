7be0ad5eaa2b54d644bb73a0af3193af07b1eac5
Bryan Kribbs <bkribbs15@gmail.com>
2021-04-14 21:18:27 -0500

    Update kafkaConnection.js

--- src/config/kafkaConnection.js
+++ src/config/kafkaConnection.js
@@ -1,14 +1,14 @@
-const config = {
-    clientId: 'inventory-service-client',
-    kafka_topic: 'testTopic',
-    brokers: ['192.168.50.11:9092'],
-    connectionTimeout: 3000,
-    authenticationTimeout: 1000,
-    reauthenticationThreshold: 10000,
-    retry: {
-        retries:3,
-        maxRetryTime:5
-    }
-};
+// const config = {
+//     clientId: 'inventory-service-client',
+//     kafka_topic: 'testTopic',
+//     brokers: ['192.168.50.11:9092'],
+//     connectionTimeout: 3000,
+//     authenticationTimeout: 1000,
+//     reauthenticationThreshold: 10000,
+//     retry: {
+//         retries:3,
+//         maxRetryTime:5
+//     }
+// };
 
-exports.config = config
+//exports.config = config
