dbf9b2f7be872e4aa54410da863e16bd4b720f25
Bryan Kribbs <bkribbs15@gmail.com>
2021-05-06 13:52:36 -0500

    Update consumer.js

--- src/kafka/consumer.js
+++ src/kafka/consumer.js
@@ -8,3 +8,31 @@ var topic = opts.topic;
 const kafka = new Kafka(opts)
 const consumer = kafka.consumer()
 
+
+function consumeMessage(){
+    try{
+        console.log('Connecting Consumer');
+        await consumer.connect()
+        console.log('Consumer Connected');
+    } catch(e){
+
+    }
+    try{
+        await consumer.subscribe({ topic: opts.topic })
+        await consumer.run({
+            eachMessage: async ({ topic, partition, message }) => {
+                console.log({
+                    key: message.key.toString(),
+                    value: message.value.toString(),
+                    headers: message.headers,
+                })
+            },
+        })
+    } catch(e){
+
+    }
+}
+
+
+// It's possible to start from the beginning of the topic
+await consumer.subscribe({ topic: 'topic-D', fromBeginning: true })
